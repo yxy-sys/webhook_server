1d47816f09b43b8ab333518a3a5589754d4f6dc7
yxy-sys <txing365@gmail.com>
2025-08-04 16:38:16 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+VERIFY_TOKEN = "ebay_webhook_verify"  # 用户指定的 Verification Token
+
+@app.route("/", methods=["GET"])
+def verify():
+    challenge = request.args.get("challenge_code")
+    verify_token = request.args.get("verify_token")
+    if verify_token == VERIFY_TOKEN:
+        return challenge, 200
+    return "Unauthorized", 401
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
